d6b351df22c9169eedf7fb2ddd13bc4623efab63
seba3567 <sebastian26481@gmail.com>
2022-04-17 21:27:30 -0400

    fix sys link

--- Prueba/settings.py
+++ Prueba/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Prueba.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["C:/entornos/Prueba/Prueba],
+        'DIRS': ["C:/entornos/Prueba/Prueba/web"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
