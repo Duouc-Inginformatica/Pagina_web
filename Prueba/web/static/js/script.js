645a5d1739be94bdf514371e7fed07a895285322
seba3567 <sebastian26481@gmail.com>
2022-04-19 13:34:50 -0400

    14.1 | move search bar to head     - update script for search bar

--- Prueba/web/static/js/script.js
+++ Prueba/web/static/js/script.js
@@ -1,7 +1,7 @@
 const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
     toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
+    searchBtn = body.querySelector(".search-box_1"),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
@@ -11,12 +11,17 @@ toggle.addEventListener("click", () => {
 })
 
 searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
+    if (sidebar.classList.contains("close")) {
+        // console.log("la barra de navegacion esta cerrada");
+    } else {
+        sidebar.classList.toggle("close");
+        // console.log("Se fuerza el cierre de la barra de navegacion");
+    }
 })
+
 
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
-
     if (body.classList.contains("dark")) {
         modeText.innerText = "Modo oscuro ";
     } else {
