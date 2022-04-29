7b764095aad59bda39527e2bcee47895283f5c9c
seba3567 <sebastian26481@gmail.com>
2022-04-28 23:47:22 -0400

    Revert "que son las cokkies?"
    This reverts commit f5ebfc884cd1d84c64248c9586eb3c5a82e7a4b9.

--- Prueba/urls.py
+++ Prueba/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from unittest.mock import patch
 from django.contrib import admin
 from django.urls import path
-from Prueba.vista import index, cliente, compras, consolas, juegos, portatiles, registro, cokies
+from Prueba.vista import index, cliente, compras, consolas, juegos, portatiles, registro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +27,4 @@ urlpatterns = [
     path('portatiles', portatiles, name='portatiles'),
     path('cliente', cliente, name='cliente'),
     path('registro', registro, name='registro'),
-    patch('cokies', cokies, name='cokies'),
 ]
