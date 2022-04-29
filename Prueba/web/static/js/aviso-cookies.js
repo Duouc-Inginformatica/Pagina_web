0d1e7792f1e688450f293cf2e167e0e04e2d87e0
seba3567 <sebastian26481@gmail.com>
2022-04-29 12:53:59 -0400

    improve rute for cockies

--- Prueba/web/static/js/aviso-cookies.js
+++ Prueba/web/static/js/aviso-cookies.js
@@ -4,6 +4,26 @@ const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
 dataLayer = [];
 
+function confcokkies() {
+    swal({
+        title: "Cookies aceptadas",
+        text: "Usted acepto las cookies!",
+        type: "success",
+        position: "top-end",
+        closeOnConfirm: false
+    });
+}
+
+function confcokkies2() {
+    swal({
+        title: "Cookies aceptadas",
+        text: "Usted acepto las cookies!",
+        type: "success",
+        position: "top-end",
+        closeOnConfirm: false
+    });
+}
+
 if (!localStorage.getItem('cookies-aceptadas')) {
     avisoCookies.classList.add('activo');
     fondoAvisoCookies.classList.add('activo');
@@ -14,6 +34,7 @@ if (!localStorage.getItem('cookies-aceptadas')) {
 botonAceptarCookies.addEventListener('click', () => {
     avisoCookies.classList.remove('activo');
     fondoAvisoCookies.classList.remove('activo');
+    confcokkies();
 
     localStorage.setItem('cookies-aceptadas', true);
 
