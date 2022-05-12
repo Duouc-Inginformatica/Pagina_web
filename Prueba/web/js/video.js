2c421ba28b0802493d8ade66846a368e7c2d7f0a
pablogonso <pablogonso@gmail.com>
2022-05-12 00:40:51 -0400

    Primera version de carrusel e imgaenes de juegos

--- Prueba/web/js/video.js
+++ Prueba/web/js/video.js
@@ -18,3 +18,25 @@ function valorant() {
     confirmButtonAriaLabel: 'Me Gusta, Genial!',
   })
 }
+
+
+function gta() {
+  Swal.fire({
+  title: '<strong>Valorant <u>TRAILER</u></strong>',
+  titleTextColor: 'var(--primary-color-light)',
+  icon: '<img src="images/mando.png">',
+  html:
+    '<iframe width="480" height="315" src="https://www.youtube.com/embed/QkkoHAzjnUs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+  showCloseButton: true,
+  showCancelButton: false,
+  focusConfirm: false,
+  confirmButtonTextcolor: 'var(--primary-color-light)',
+  confirmButtonColor: 'var(--primary-color)',
+  background: [
+    statusnegro(),
+],
+  confirmButtonText:
+    '<i class="fa fa-thumbs-up"></i> Genia!',
+  confirmButtonAriaLabel: 'Me Gusta, Genial!',
+})
+}
