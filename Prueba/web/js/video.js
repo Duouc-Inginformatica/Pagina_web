a401d6bccf6a2b95a62cc49950a3daaa56a7217d
seba3567 <sebastian26481@gmail.com>
2022-05-15 18:16:15 -0400

    drop django 2

--- Prueba/web/js/video.js
+++ Prueba/web/js/video.js
@@ -1,17 +1,20 @@
 function valorant() {
     Swal.fire({
     title: '<strong>Valorant <u>TRAILER</u></strong>',
-    icon: 'info',
+    titleTextColor: 'var(--primary-color-light)',
+    icon: '<img src="images/mando.png">',
     html:
-      '<iframe width="560" height="315" src="{% static "https://www.youtube.com/embed/Ey770ez6ROk" autoplay;  picture-in-picture" allowfullscreen></iframe>',
+      '<iframe width="480" height="315" src="https://www.youtube.com/embed/Ey770ez6ROk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     showCloseButton: true,
-    showCancelButton: true,
+    showCancelButton: false,
     focusConfirm: false,
+    confirmButtonTextcolor: 'var(--primary-color-light)',
+    confirmButtonColor: 'var(--primary-color)',
+    background: [
+      statusnegro(),
+  ],
     confirmButtonText:
       '<i class="fa fa-thumbs-up"></i> Genia!',
     confirmButtonAriaLabel: 'Me Gusta, Genial!',
-    cancelButtonText:
-      '<i class="fa fa-thumbs-down"></i>',
-    cancelButtonAriaLabel: 'No me gusta'
   })
 }
